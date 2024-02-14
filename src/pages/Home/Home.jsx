539900ef1b3902e1556668b69d3421d08bbee5e2
Muslim Mageza <muslimmageza@MacBook-Pro-Muslim.local>
2024-02-14 17:38:24 +0600

    finish

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import First from "./First/First";
+import Comp from "./Comp/Comp";
+import Izd from "./Izd/Izd";
+
+const Home = () => {
+    return (
+        <>
+            <First/>
+            <Comp/>
+            <Izd/>
+        </>
+    );
+};
+
+export default Home;
---
+++ src/pages/Home/news/News.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const News = () => {
+    return (
+        <section className="new">
+            <div className="container">
+                <h2 className="new__title">
+                    НОВОСТИ
+                </h2>
+                <div className="new__info"></div>
+            </div>
+        </section>
+    );
+};
+
+export default News;
